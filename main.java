f2c2a521278aeb7c6bd6de9324eaf9dd72be3c3b
jjchoiosc <jj.choi@osckorea.com>
2024-11-26 15:31:21 +0900

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+// Main.java
+public class Main {
+    public static String getMessage() {
+        return "Hello, CI/CD!";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
